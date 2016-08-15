03fecf00ab572eef9537efc349cb1cf183ccc324
clerkma <clerkma@gmail.com>
2016-08-15 15:40:24 +0800

    updated fontconfig.

--- src/fontconfig-2.12.1/src/fcstdint.h
+++ src/fontconfig-2.12.1/src/fcstdint.h
@@ -2,7 +2,7 @@
 #define _FONTCONFIG_SRC_FCSTDINT_H 1
 #ifndef _GENERATED_STDINT_H
 #define _GENERATED_STDINT_H "fontconfig 2.12.1"
-/* generated using gnu compiler gcc (GCC) 6.1.1 20160621 (Red Hat 6.1.1-3) */
+/* generated using gnu compiler gcc (Ubuntu 5.4.0-6ubuntu1~16.04.2) 5.4.0 20160609 */
 #define _STDINT_HAVE_STDINT_H 1
 #include <stdint.h>
 #endif
